4d521414ff6409af2a9372a718a460a5d06fce40
Pavel Nikitinskiy <pnikitinskiy@pixsystem.com>
2016-08-31 19:21:38 +0300

    resolved issue with images

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,17 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Pavel Nikitinskiy on 8/31/16.
+//  Copyright © 2016 pavel. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
